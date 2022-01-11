4f576d49a7873b0ab25b67af30c16b634610afbc
Ted Dino <ted23@tutanota.com>
2022-01-11 21:20:03 +0800

    add smart navbar

--- edie-homepage/app.js
+++ edie-homepage/app.js
@@ -1,13 +1,80 @@
 const nav = document.querySelector(".nav-items");
 const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelectorAll(".nav-link");
+
+let visibility;
 
 navToggle.addEventListener("click", () => {
-  const visibility = nav.getAttribute("data-visible");
+  visibility = nav.getAttribute("data-visible");
   if (visibility === "false") {
     nav.setAttribute("data-visible", true);
     navToggle.setAttribute("aria-expanded", true);
   } else {
     nav.setAttribute("data-visible", false);
     navToggle.setAttribute("aria-expanded", false);
+  }
+});
+
+links.forEach((link) => {
+  console.log(link);
+  link.addEventListener("click", () => {
+    visibility = nav.getAttribute("data-visible");
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  });
+});
+
+// Creating a Smart Navbar With Vanilla JavaScript
+// https://css-tricks.com/creating-a-smart-navbar-with-vanilla-javascript/
+const header = document.querySelector("header");
+const supportPageOffset = window.pageXOffset !== undefined;
+const isCSS1Compat = (document.compatMode || "") === "CSS1Compat";
+
+let previousScrollPosition = 0;
+
+const isScrollingDown = () => {
+  let scrolledPosition = supportPageOffset
+    ? window.pageYOffset
+    : isCSS1Compat
+    ? document.documentElement.scrollTop
+    : document.body.scrollTop;
+  let isScrollDown;
+
+  if (scrolledPosition > previousScrollPosition) {
+    isScrollDown = true;
+  } else {
+    isScrollDown = false;
+  }
+  previousScrollPosition = scrolledPosition;
+  return isScrollDown;
+};
+
+const handleNavScroll = () => {
+  if (isScrollingDown() && !header.contains(document.activeElement)) {
+    header.classList.add("scroll-down");
+    header.classList.remove("scroll-up");
+  } else {
+    header.classList.add("scroll-up");
+    header.classList.remove("scroll-down");
+  }
+};
+
+var throttleTimer;
+
+const throttle = (callback, time) => {
+  if (throttleTimer) return;
+
+  throttleTimer = true;
+  setTimeout(() => {
+    callback();
+    throttleTimer = false;
+  }, time);
+};
+
+const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+
+window.addEventListener("scroll", () => {
+  if (mediaQuery && !mediaQuery.matches) {
+    throttle(handleNavScroll, 250);
   }
 });
