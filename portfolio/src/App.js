a52fc1c6226d5cf357e18a1454c31322ef3f65d7
Ted Dino <ted23@tutanota.com>
2022-01-17 20:00:18 +0800

    add personal info

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+import PersonalInfo from "./components/PersonalInfo";
+
+function App() {
+  return (
+    <div>
+      <PersonalInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/PersonalInfo.js
@@ -0,0 +1,14 @@
+export default function PersonalInfo() {
+  return (
+    <section className="info">
+      <div>
+        <img
+          src="https://source.unsplash.com/random/?male"
+          aria-hidden="true"
+          loading="lazy"
+          alt=""
+        />
+      </div>
+    </section>
+  );
+}
